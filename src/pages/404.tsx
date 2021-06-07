7e315ee1f9974d26774e696ce85850f932351a33
Nicolas M. Pardo <nicolasm@nikodermus.media>
2021-06-06 19:13:27 -0500

    fix(routing): enable 404 server side

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,33 @@
+import { Box, Typography } from '@material-ui/core';
+import styled from 'styled-components';
+import Link from '../components/Link';
+import logo from '../images/logo.png';
+
+const StyledImg = styled.img`
+  width: 150px;
+  padding: 24px 0;
+`;
+
 const NotFound: React.FC = () => {
-  return <div>a</div>;
+  return (
+    <Box py={10} margin="auto" maxWidth={350} textAlign="center">
+      <Link to="/">
+        <StyledImg src={logo} alt="A1 Logo" />
+      </Link>
+
+      <Typography gutterBottom variant="h4">
+        404 - Not Found
+      </Typography>
+      <Typography>
+        Sorry, the page you are looking for does not exist. You can always go
+        back to the
+        <Link to="/" color="primary">
+          {` homepage`}
+        </Link>
+        .
+      </Typography>
+    </Box>
+  );
 };
 
 export default NotFound;
